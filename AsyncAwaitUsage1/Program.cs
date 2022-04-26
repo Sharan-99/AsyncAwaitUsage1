31ba7d60007b1a26e9c280dc256972e1f43cbca5
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-04-26 11:40:22 +0530

    first commit

---
+++ AsyncAwaitUsage1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitUsage1
+{
+    class Program
+    {
+        private string Method2()
+        {
+            string s = "Hi,Welcome to Async and Await Concept of C#";
+            Thread.Sleep(10000);
+            return s;
+        }
+        private async void Method1()
+        {
+            Task<string> task = new Task<string>(Method2);
+            task.Start();
+            //string str = Method2();
+            string str = await task;
+            Console.WriteLine(str);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Method1();
+            Console.ReadKey();
+        }
+    }
+}
